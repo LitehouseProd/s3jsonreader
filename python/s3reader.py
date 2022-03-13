738eddb4a61bb1de0ebabc18075d8bf31cdd9095
willschottler <71202742+willschottler@users.noreply.github.com>
2022-03-13 00:22:37 -0500

    debug: see what this gives me

--- python/s3reader.py
+++ python/s3reader.py
@@ -10,7 +10,7 @@ def getLocation(url):
 def readFile(fileLocation):
     with open(fileLocation, "r") as read_file:
         data = json.load(read_file)
-        print(data)
+        print(data.items())
 
 sourceArray = getLocation('s3://ecr-test-fiftheyeecrtest-1cc7embghgf27/test.json')
 file = getFile(sourceArray[2], sourceArray[3])
