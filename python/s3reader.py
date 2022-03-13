58d41db733ec4680fd77af12679502b328fee991
willschottler <71202742+willschottler@users.noreply.github.com>
2022-03-13 00:33:07 -0500

    debug: see if this gets both cases

--- python/s3reader.py
+++ python/s3reader.py
@@ -10,12 +10,14 @@ def getLocation(url):
 def readFile(fileLocation):
     with open(fileLocation, "r") as read_file:
         data = json.load(read_file)
-        # for i in data.items():
-        #     if type(i) is dict:
-        #         for j in i.items():
-        #             if j is 'message':
-        #                 print(j['message'])
-        print(data['message'])
+        for i in data.items():
+            if type(i) is dict:
+                for j in i.items():
+                    if j is 'message':
+                        print(j['message'])
+            else:
+                if i is 'message':
+                    print(i['message'])
 
 sourceArray = getLocation('s3://ecr-test-fiftheyeecrtest-1cc7embghgf27/test2.json')
 file = getFile(sourceArray[2], sourceArray[3])
