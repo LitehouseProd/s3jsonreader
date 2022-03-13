48b8678462129e2b0ce35489b354b96bd7163e1e
willschottler <71202742+willschottler@users.noreply.github.com>
2022-03-13 11:49:14 -0400

    added unit test for url parse, added test step to pipeline

--- python/tests.py
+++ python/tests.py
@@ -22,3 +22,6 @@ def testLoad2():
     with open('../jsonfile/test2.json','r') as readfile:
         data = json.load(readfile)
         assert readFile('../jsonfile/test2.json') == data
+
+def testURLParse():
+    assert getLocation('s3://ecr-test-fiftheyeecrtest-1cc7embghgf27/test2.json') == ['s3:', '', 'ecr-test-fiftheyeecrtest-1cc7embghgf27', 'test2.json']
