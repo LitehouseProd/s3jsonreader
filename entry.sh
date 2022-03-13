8a5b7be8667f55ac212ae43861560e206609516b
willschottler <wschottler@wschottler.com>
2022-03-13 18:12:41 -0400

    fix: aws calls

--- entry.sh
+++ entry.sh
@@ -1,8 +1,8 @@
 
 #!/bin/bash
 
-/usr/local/bin/aws configure set aws_access_key_id $ACCESS_KEY_ID
-/usr/local/bin/aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
-/usr/local/bin/aws configure set default.region us-east-1
+aws configure set aws_access_key_id $ACCESS_KEY_ID
+aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
+aws configure set default.region us-east-1
 
 python3 ./python/main.py $FILE_URI
