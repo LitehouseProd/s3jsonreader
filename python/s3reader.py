a1985618e5bc7aeea1079fde8c2a3d6ccb8e3ce9
willschottler <71202742+willschottler@users.noreply.github.com>
2022-03-13 01:09:17 -0500

    fix: it is like it doesn't even see the function is called

--- python/s3reader.py
+++ python/s3reader.py
@@ -7,6 +7,12 @@ def getLocation(url):
     source=url.split('/')
     return source
 
+def readFile(fileLocation):
+    with open(fileLocation, "r") as read_file:
+        data = json.load(read_file)
+        print("file is read")
+        return data
+
 def getAllkeys(info):
     print("get_all_keys?")
     for key, value in info.items():
@@ -17,12 +23,6 @@ def getAllkeys(info):
         elif isinstance(value, dict):
             print("elif")
             yield from getAllkeys(value)
-
-def readFile(fileLocation):
-    with open(fileLocation, "r") as read_file:
-        data = json.load(read_file)
-        print("file is read")
-        return data
         
 
 sourceArray = getLocation('s3://ecr-test-fiftheyeecrtest-1cc7embghgf27/test2.json')
