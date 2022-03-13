188d52eb97a3605ca6f61e16a5a2e8d7845184ad
willschottler <71202742+willschottler@users.noreply.github.com>
2022-03-13 12:48:03 -0400

    fix: two tests were set up wrong

--- python/tests.py
+++ python/tests.py
@@ -20,12 +20,12 @@ def testRead2():
 def testLoad():
     with open(file1,'r') as readfile:
         data = json.load(readfile)
-        assert readFile('../jsonfile/test.json') == data
+        assert readFile(file1) == data
 
 def testLoad2():
     with open(file2,'r') as readfile:
         data = json.load(readfile)
-        assert readFile('../jsonfile/test2.json') == data
+        assert readFile(file2) == data
 
 def testURLParse():
     assert getLocation('s3://ecr-test-fiftheyeecrtest-1cc7embghgf27/test2.json') == ['s3:', '', 'ecr-test-fiftheyeecrtest-1cc7embghgf27', 'test2.json']
