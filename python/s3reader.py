f36ffc04776972fd9ef162fc43d5f42a34268680
willschottler <71202742+willschottler@users.noreply.github.com>
2022-03-13 00:28:10 -0500

    debug: this wouldn't happen to work would it

--- python/s3reader.py
+++ python/s3reader.py
@@ -10,7 +10,12 @@ def getLocation(url):
 def readFile(fileLocation):
     with open(fileLocation, "r") as read_file:
         data = json.load(read_file)
-        print(data.items())
+        # for i in data.items():
+        #     if type(i) is dict:
+        #         for j in i.items():
+        #             if j is 'message':
+        #                 print(j['message'])
+        print(data['message'])
 
 sourceArray = getLocation('s3://ecr-test-fiftheyeecrtest-1cc7embghgf27/test.json')
 file = getFile(sourceArray[2], sourceArray[3])
