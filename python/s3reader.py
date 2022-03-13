2355b8eb4c82dd0b1c37a9ff51030505bb8cf39c
willschottler <71202742+willschottler@users.noreply.github.com>
2022-03-13 00:39:43 -0500

    fix: this is better patterning for this

--- python/s3reader.py
+++ python/s3reader.py
@@ -7,17 +7,19 @@ def getLocation(url):
     source=url.split('/')
     return source
 
+def get_all_keys(Dict):
+    for key, value in Dict.items():
+            yield key
+            if isinstance(value, dict):
+                yield from get_all_keys(value)
+
 def readFile(fileLocation):
     with open(fileLocation, "r") as read_file:
         data = json.load(read_file)
-        for i in data.items():
-            if type(i) is dict:
-                for j in i.items():
-                    if j == 'message':
-                        print(j['message'])
-            else:
-                if i == 'message':
-                    print(i['message'])
+        for i in get_all_keys(data):
+            if i == 'message':
+                print(i['message'])
+        
 
 sourceArray = getLocation('s3://ecr-test-fiftheyeecrtest-1cc7embghgf27/test2.json')
 file = getFile(sourceArray[2], sourceArray[3])
