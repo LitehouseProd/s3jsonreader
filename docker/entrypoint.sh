035860b13e624c31c92fcb46511c40cea8697cbb
willschottler <71202742+willschottler@users.noreply.github.com>
2022-03-13 11:11:07 -0400

    added cli argument toi entrypoint file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,4 +3,4 @@
 /usr/local/bin/aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
 /usr/local/bin/aws configure set default.region us-east-1
 
-python3 ./python/s3reader.py
+python3 ./python/s3reader.py $FILE_ID
