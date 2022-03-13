3727cfb0d36da7c9e40fc52147a204f812f725ef
willschottler <71202742+willschottler@users.noreply.github.com>
2022-03-12 22:24:09 -0500

    feat: added requirements file, set up circleci builder, added intial code to parse s3 source

---
+++ python/s3reader.py
@@ -0,0 +1,14 @@
+import boto3;
+import json;
+from getCreds import get_secret;
+
+s3 = boto3.resource('s3')
+
+def getFile(Bucket, file):
+    s3.Object(Bucket, file).downloadfile('/tmp/test.json')
+
+def getLocation(url):
+    source=url.split('/')
+    print(url)
+
+getLocation('s3://ecr-test-fiftheyeecrtest-1cc7embghgf27/test.json')
