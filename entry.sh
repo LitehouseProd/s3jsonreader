802052367c2446a5128a9f8b6828170dbd85c09a
willschottler <wschottler@wschottler.com>
2022-03-13 21:36:47 -0400

    need to find out why this is failing to run on a built image with docker-compose

--- entry.sh
+++ entry.sh
@@ -4,5 +4,5 @@
 aws configure set aws_access_key_id $ACCESS_KEY_ID
 aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
 aws configure set default.region us-east-1
-
+echo $FILE_URI
 python3 main.py $FILE_URI
