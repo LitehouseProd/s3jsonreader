2b30005ee1da2c318a5de6751a621d4afaee3845
willschottler <wschottler@wschottler.com>
2022-03-13 21:40:21 -0400

    try passing the variables this way

--- entry.sh
+++ entry.sh
@@ -1,8 +1,12 @@
 
 #!/bin/bash
 
+FILE_URI=$1
+ACCESS_KEY_ID=$2
+SECRET_ACCESS_KEY=$3
+
 aws configure set aws_access_key_id $ACCESS_KEY_ID
 aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
 aws configure set default.region us-east-1
-echo $FILE_URI
+
 python3 main.py $FILE_URI
